520bd05534168dfc41de8a3ec65c9840e792103b
Patrick Hermann <patrick.hermann@sva.de>
2024-11-12 22:22:42 +0000

    updated crossplane

--- crossplane/main.go
+++ crossplane/main.go
@@ -15,17 +15,14 @@ type Crossplane struct {
 }
 
 // Init Crossplane Package
-func (m *Crossplane) InitPackage(ctx context.Context, name string) {
+func (m *Crossplane) InitPackage(ctx context.Context, name string) *dagger.Directory {
 
-	output, err := m.XplaneContainer.
-		WithWorkdir("/project").
+	output := m.XplaneContainer.
 		WithExec(
-			[]string{"crossplane", "xpkg", "init", name, "configuration-template"}).
-		Stdout(ctx)
-
-	fmt.Println(err)
+			[]string{"crossplane", "xpkg", "init", name, "configuration-template", "-d", name})
 
 	fmt.Println(output)
+	return output.Directory(name)
 }
 
 // GetXplaneContainer return the default image for helm
