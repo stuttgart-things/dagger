ab9bde726d9a72a8d9c604fb4f53b7d2188e2c3a
Patrick Hermann <patrick.hermann@sva.de>
2024-11-18 14:30:00 +0000

    updated

--- crossplane/main.go
+++ crossplane/main.go
@@ -7,8 +7,8 @@ package main
 import (
 	"context"
 	"dagger/crossplane/internal/dagger"
+	reg "dagger/crossplane/registry"
 	"dagger/crossplane/templates"
-
 	"fmt"
 )
 
@@ -35,6 +35,41 @@ func (m *Crossplane) Package(ctx context.Context, src *dagger.Directory) *dagger
 	fmt.Println("BUILD PACKAGE: ", buildArtifact)
 
 	return xplane.Directory("/src")
+}
+
+// Push Crossplane Package
+func (m *Crossplane) Push(
+	ctx context.Context,
+	src *dagger.Directory,
+	// +default="ghcr.io"
+	registry string,
+	username string,
+	password *dagger.Secret,
+	destination string) string {
+
+	dirWithPackage := m.Package(ctx, src)
+
+	passwordPlaintext, err := password.Plaintext(ctx)
+
+	configJSON, err := reg.CreateDockerConfigJSON(username, passwordPlaintext, registry)
+	if err != nil {
+		fmt.Printf("ERROR CREATING DOCKER config.json: %v\n", err)
+	}
+
+	status, err := m.XplaneContainer.
+		WithNewFile("/root/.docker/config.json", configJSON).
+		WithDirectory("/src", dirWithPackage).
+		WithWorkdir("/src").
+		WithExec([]string{"crossplane", "xpkg", "push", destination}).
+		Stdout(ctx)
+
+	if err != nil {
+		fmt.Println("ERROR PUSHING PACKAGE: ", err)
+	}
+
+	fmt.Println("PACKAGE STATUS: ", status)
+
+	return status
 }
 
 // GetXplaneContainer return the default image for helm
