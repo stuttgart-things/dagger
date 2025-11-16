28f66632dc1f7bc1608e8b0e55654357130a718a
Patrick Hermann <patrick.hermann@sva.de>
2025-11-16 12:32:16 +0000

    feat: feat/add-talk-with-k8s

--- helm/container.go
+++ helm/container.go
@@ -8,50 +8,71 @@ import (
 func (m *Helm) container() *dagger.Container {
 	arch := "linux_amd64"
 
-	helmfileVersion := "1.1.7"
-	helmfileBin := "helmfile"
-	helmfileTar := fmt.Sprintf("%s_%s_%s.tar.gz", helmfileBin, helmfileVersion, arch)
-	helmfileURL := fmt.Sprintf("https://github.com/helmfile/helmfile/releases/download/v%s/%s", helmfileVersion, helmfileTar)
-	helmfileBinPath := "/usr/bin/" + helmfileBin
-
-	polarisVersion := "10.1.1"
-	polarisTar := fmt.Sprintf("polaris_%s.tar.gz", arch)
-	polarisURL := fmt.Sprintf("https://github.com/FairwindsOps/polaris/releases/download/%s/polaris_linux_amd64.tar.gz", polarisVersion)
-	polarisBinPath := "/usr/bin/polaris"
-
-	valsVersion := "0.42.1"
-	valsBin := "vals"
-	valsTar := fmt.Sprintf("%s_%s_%s.tar.gz", valsBin, valsVersion, arch)
-	valsURL := fmt.Sprintf("https://github.com/helmfile/vals/releases/download/v%s/%s", valsVersion, valsTar)
-	valsBinPath := "/usr/bin/" + valsBin
-
+	// --- BASE IMAGE ---
 	if m.BaseImage == "" {
 		m.BaseImage = "cgr.dev/chainguard/wolfi-base:latest"
 	}
 
 	ctr := dag.Container().
-		From(m.BaseImage)
+		From(m.BaseImage).
+		WithExec([]string{"apk", "add", "--no-cache", "wget", "curl", "git", "kubectl"})
 
-	ctr = ctr.WithExec([]string{"apk", "add", "--no-cache", "wget", "helm", "git"})
+	// ======================================================
+	// INSTALL HELM (manual: Wolfi does NOT ship Helm 3.x)
+	// ======================================================
+	helmVersion := "v3.19.2"
 
+	ctr = ctr.
+		WithExec([]string{"apk", "add", "--no-cache", "wget", "curl", "git"}).
+		WithExec([]string{"sh", "-c", "wget https://get.helm.sh/helm-" + helmVersion + "-linux-amd64.tar.gz -O /tmp/helm.tar.gz"}).
+		WithExec([]string{"tar", "-xzvf", "/tmp/helm.tar.gz", "-C", "/tmp/"}).
+		WithExec([]string{"mv", "/tmp/linux-amd64/helm", "/usr/bin/helm"}).
+		WithExec([]string{"chmod", "+x", "/usr/bin/helm"})
+
+	// ======================================================
 	// INSTALL HELMFILE
-	ctr = ctr.WithExec([]string{"wget", "-O", "/tmp/" + helmfileTar, helmfileURL})
-	ctr = ctr.WithExec([]string{"tar", "-xzf", "/tmp/" + helmfileTar, "-C", "/tmp/"})
-	ctr = ctr.WithExec([]string{"mv", fmt.Sprintf("/tmp/%s", helmfileBin), helmfileBinPath})
-	ctr = ctr.WithExec([]string{"chmod", "+x", helmfileBinPath})
-	ctr = ctr.WithExec([]string{"helmfile", "init", "--force"})
+	// ======================================================
+	helmfileVersion := "1.1.9"
+	helmfileBin := "helmfile"
+	helmfileTar := fmt.Sprintf("%s_%s_%s.tar.gz", helmfileBin, helmfileVersion, arch)
+	helmfileURL := fmt.Sprintf("https://github.com/helmfile/helmfile/releases/download/v%s/%s", helmfileVersion, helmfileTar)
+	helmfileBinPath := "/usr/bin/" + helmfileBin
 
+	ctr = ctr.
+		WithExec([]string{"wget", "-O", "/tmp/" + helmfileTar, helmfileURL}).
+		WithExec([]string{"tar", "-xzf", "/tmp/" + helmfileTar, "-C", "/tmp/"}).
+		WithExec([]string{"mv", "/tmp/" + helmfileBin, helmfileBinPath}).
+		WithExec([]string{"chmod", "+x", helmfileBinPath}).
+		WithExec([]string{"helmfile", "init", "--force"})
+
+	// ======================================================
 	// INSTALL POLARIS
-	ctr = ctr.WithExec([]string{"wget", "-O", "/tmp/" + polarisTar, polarisURL})
-	ctr = ctr.WithExec([]string{"tar", "-xzf", "/tmp/" + polarisTar, "-C", "/tmp/"})
-	ctr = ctr.WithExec([]string{"mv", "/tmp/polaris", polarisBinPath})
-	ctr = ctr.WithExec([]string{"chmod", "+x", polarisBinPath})
+	// ======================================================
+	polarisVersion := "10.1.2"
+	polarisTar := fmt.Sprintf("polaris_%s.tar.gz", arch)
+	polarisURL := fmt.Sprintf("https://github.com/FairwindsOps/polaris/releases/download/%s/polaris_linux_amd64.tar.gz", polarisVersion)
+	polarisBinPath := "/usr/bin/polaris"
 
+	ctr = ctr.
+		WithExec([]string{"wget", "-O", "/tmp/" + polarisTar, polarisURL}).
+		WithExec([]string{"tar", "-xzf", "/tmp/" + polarisTar, "-C", "/tmp/"}).
+		WithExec([]string{"mv", "/tmp/polaris", polarisBinPath}).
+		WithExec([]string{"chmod", "+x", polarisBinPath})
+
+	// ======================================================
 	// INSTALL VALS
-	ctr = ctr.WithExec([]string{"wget", "-O", "/tmp/" + valsTar, valsURL})
-	ctr = ctr.WithExec([]string{"tar", "-xzf", "/tmp/" + valsTar, "-C", "/tmp/"})
-	ctr = ctr.WithExec([]string{"mv", fmt.Sprintf("/tmp/%s", valsBin), valsBinPath})
-	ctr = ctr.WithExec([]string{"chmod", "+x", valsBinPath})
+	// ======================================================
+	valsVersion := "0.42.4"
+	valsBin := "vals"
+	valsTar := fmt.Sprintf("%s_%s_%s.tar.gz", valsBin, valsVersion, arch)
+	valsURL := fmt.Sprintf("https://github.com/helmfile/vals/releases/download/v%s/%s", valsVersion, valsTar)
+	valsBinPath := "/usr/bin/" + valsBin
+
+	ctr = ctr.
+		WithExec([]string{"wget", "-O", "/tmp/" + valsTar, valsURL}).
+		WithExec([]string{"tar", "-xzf", "/tmp/" + valsTar, "-C", "/tmp/"}).
+		WithExec([]string{"mv", "/tmp/" + valsBin, valsBinPath}).
+		WithExec([]string{"chmod", "+x", valsBinPath})
 
 	return ctr
 }
