55ab98e6aeeb1d787b25a1779af26a7ebeaf61d6
Patrick Hermann <patrick.hermann@sva.de>
2025-11-16 12:32:16 +0000

    feat: feat/add-talk-with-k8s

---
+++ helm/config.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"dagger/helm/internal/dagger"
+	"fmt"
+	"strings"
+)
+
+func (m *Helm) TalkWithKubernetes(
+	ctx context.Context,
+	// src *dagger.Directory,
+	// +optional
+	// +default="get"
+	operation string,
+	// +optional
+	// +default="pods"
+	resourceKind string,
+	// +optional
+	namespace string,
+	// +optional
+	kubeConfig *dagger.Secret,
+	// +optional
+	additionalCommand string,
+) (string, error) {
+
+	kubeConfigPath := "/root/.kube/config"
+
+	// Build kubectl command arguments
+	args := []string{"kubectl", operation, resourceKind}
+
+	// Only add namespace flag if namespace is provided
+	if namespace != "" {
+		args = append(args, "-n", namespace)
+	}
+
+	kubectlContainer := m.container().
+		WithMountedSecret(kubeConfigPath, kubeConfig)
+
+	// If additional command is provided, pipe kubectl output through it
+	if additionalCommand != "" {
+		// Build a shell command that pipes kubectl through the additional command
+		fullCommand := fmt.Sprintf("%s | %s", strings.Join(args, " "), additionalCommand)
+		kubectlContainer = kubectlContainer.WithExec([]string{"sh", "-c", fullCommand})
+	} else {
+		// Execute kubectl command directly
+		kubectlContainer = kubectlContainer.WithExec(args)
+	}
+
+	// Run the command and capture output
+	out, err := kubectlContainer.Stdout(ctx)
+	if err != nil {
+		// also capture stderr for debugging
+		stderr, _ := kubectlContainer.Stderr(ctx)
+		return "", fmt.Errorf("kubectl error: %w\nstderr: %s", err, stderr)
+	}
+
+	return out, nil
+}
